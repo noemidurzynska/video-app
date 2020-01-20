5f953ab9af2f51e9e2682901f70afadec654d454
Noemi Durżyńska <noemidurzynska@gmail.com>
2020-01-20 16:39:39 +0100

    initial commit

---
+++ video-app/src/app/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { PageEvent } from '@angular/material/paginator';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.less']
+})
+export class HomeComponent implements OnInit {
+  // MatPaginator Inputs
+  length = 100;
+  pageSize = 10;
+  pageSizeOptions: number[] = [5, 10, 25, 100];
+
+  // MatPaginator Output
+  pageEvent: PageEvent;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
