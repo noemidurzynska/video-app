347c3f76185640bb5bd2dbb9818be1dafd56a41a
Noemi Durżyńska <noemidurzynska@gmail.com>
2020-01-29 21:23:14 +0100

    home page commit

--- video-app/src/app/home/home.component.ts
+++ video-app/src/app/home/home.component.ts
@@ -1,13 +1,7 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { PageEvent } from '@angular/material/paginator';
 import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
-
-export interface Tile {
-  color: string;
-  cols: number;
-  rows: number;
-  text: string;
-}
+import { VideoModel, Source } from '../models';
 
 @Component({
   selector: 'app-home',
@@ -15,30 +9,64 @@ export interface Tile {
   styleUrls: ['./home.component.less']
 })
 export class HomeComponent implements OnInit {
-  // MatPaginator Inputs
-  length = 100;
+
+  length = 0;
   pageSize = 10;
   pageIndex = 0;
   pageSizeOptions: number[] = [5, 10, 25, 100];
+  public videos: VideoModel[] = [];
+  public allVideos: VideoModel[] = [];
+  viewModeValue = 'metro';
 
-  tiles: Tile[] = [
-    {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
-    {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
-    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
-    {text: 'Five', cols: 1, rows: 1, color: 'lightblue'},
-    {text: 'Six', cols: 1, rows: 1, color: 'lightgreen'},
-    {text: 'Seven', cols: 1, rows: 1, color: 'lightpink'},
-    {text: 'Eight', cols: 1, rows: 1, color: '#DDBDF1'},
-  ];
-
-  // MatPaginator Output
-  pageEvent: PageEvent;
   constructor(@Inject(LOCAL_STORAGE) private storage: StorageService) { }
 
   ngOnInit() {
-
-    let videoList = this.storage.get('video-list');
+    this.loadVideos();
   }
 
+  public onViewChange(viewMode: any): void {
+    this.viewModeValue = viewMode.value;
+    this.loadVideos();
+  }
+  private loadVideos(): void {
+    const videoList = this.storage.get('video-list');
+    this.allVideos = [];
+
+    videoList.forEach(video => {
+      const videoModel = new VideoModel();
+      videoModel.id = video.id;
+      if (video.source === Source.Youtube) {
+        videoModel.source = 'Youtube';
+        videoModel.playesCount = video.playesCount;
+      } else if (video.source === Source.Vimeo) {
+        videoModel.source = 'Vimeo';
+        videoModel.playesCount = 'unknown';
+      }
+      videoModel.title = video.title;
+      videoModel.date = video.date;
+      videoModel.image = video.image;
+      videoModel.likesCount = video.likesCount;
+      if (this.viewModeValue === 'metro') {
+        videoModel.cols = 2;
+      } else {
+        videoModel.cols = 4;
+      }
+      videoModel.rows = 1;
+
+      this.allVideos.push(videoModel);
+    });
+
+    this.sliceVideo();
+    this.length = this.videos.length;
+  }
+
+  public onPageChanged(event: PageEvent): void {
+    this.pageIndex = event.pageIndex;
+    this.pageSize = event.pageSize;
+    this.sliceVideo();
+  }
+
+  private sliceVideo(): void{
+    this.videos = this.allVideos.slice(this.pageIndex * this.pageSize, this.pageIndex * this.pageSize + this.pageSize);
+  }
 }
--- video-app/src/app/models/video-model.ts
+++ video-app/src/app/models/video-model.ts
@@ -1,13 +1,11 @@
-import { Source } from './source.enum';
-
 export class VideoModel{
   public id: string;
-  public playesCount: number;
+  public playesCount: string;
   public likesCount: number;
   public title: string;
   public image: string;
   public date: Date;
   public rows: number;
   public cols: number;
-  public source: Source;
+  public source: string;
 }
