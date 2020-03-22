040df33e87c0189e54a9a1fa5acfb0e44bebb914
Noemi Durżyńska <noemidurzynska@gmail.com>
2020-03-22 12:36:25 +0100

    Introduce Observable

--- video-app/src/app/core/youtube/youtube.service.ts
+++ video-app/src/app/core/youtube/youtube.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable} from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { Video, Passwords, Source  } from '../models';
 import { Router } from '@angular/router';
@@ -13,40 +15,38 @@ export class YoutubeService {
     ,          private readonly router: Router
     ,          private readonly streamingPlatform: StreamingPlatformService ) {}
 
-  public getYouTubeVideo(videoId: string, redirect: boolean, self: any): void {
+  public getYouTubeVideo(videoId: string): Observable<any> {
 
-    this.http.get('https://www.googleapis.com/youtube/v3/videos?id='
+    return this.http.get('https://www.googleapis.com/youtube/v3/videos?id='
       + videoId +
       '&key='
       + this.passwords.YouTube +
       '&part=snippet,contentDetails,statistics,status')
-      .subscribe((response: any) => {
+        .pipe(
+          switchMap((response: any) => {
 
-        if (response.items.length === 0) {
-          self.showErrorMessage = true;
-          return;
-        }
+            if (response.items.length === 0) {
+              return of(true);
+            }
 
-        const item = response.items[0];
+            const item = response.items[0];
 
-        const video = new Video();
-        video.sourceType = Source.Youtube;
-        video.source = 'Youtube';
-        video.id = item.id;
-        video.title = item.snippet.title;
-        video.date = item.snippet.publishedAt;
-        video.image = item.snippet.thumbnails.default.url;
-        video.playesCount = item.statistics.viewCount;
-        video.playsCountDescription = item.statistics.viewCount;
-        video.likesCount = item.statistics.likeCount;
-        video.urlCode = videoId;
-        video.creationDate = new Date();
+            const video = new Video();
+            video.sourceType = Source.Youtube;
+            video.source = 'Youtube';
+            video.id = item.id;
+            video.title = item.snippet.title;
+            video.date = item.snippet.publishedAt;
+            video.image = item.snippet.thumbnails.default.url;
+            video.playesCount = item.statistics.viewCount;
+            video.playsCountDescription = item.statistics.viewCount;
+            video.likesCount = item.statistics.likeCount;
+            video.urlCode = videoId;
+            video.creationDate = new Date();
 
-        this.streamingPlatform.saveVideo(video);
-
-        if (redirect) {
-          this.router.navigate(['/home']);
-        }
-      });
+            this.streamingPlatform.saveVideo(video);
+            return of(false);
+          })
+        );
   }
 }
--- video-app/src/app/library/add/add.component.ts
+++ video-app/src/app/library/add/add.component.ts
@@ -1,9 +1,9 @@
 import { Component, Inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { switchMap, filter } from 'rxjs/operators';
 import { NgForm } from '@angular/forms';
 import { Router } from '@angular/router';
-import { Passwords, Source, Video } from '../../core/models';
-import { LOCAL_STORAGE, StorageService } from 'ngx-webstorage-service';
+import { Passwords } from '../../core/models';
 import { YoutubeService, VimeoService, StreamingPlatformService } from '../../core';
 
 @Component({
@@ -21,8 +21,7 @@ export class AddComponent {
     ,         private readonly router: Router
     ,         private readonly youtubeService: YoutubeService
     ,         private readonly vimeoService: VimeoService
-    ,         private readonly streamingPlatform: StreamingPlatformService
-    ,         @Inject(LOCAL_STORAGE) private storage: StorageService) { }
+    ,         private readonly streamingPlatform: StreamingPlatformService) { }
 
   public onAddClick(form: NgForm, platform: any): void {
 
@@ -34,20 +33,28 @@ export class AddComponent {
 
     const videoId = this.streamingPlatform.extractIdentifier(this.apply.videoId);
     if (platform.value === 'youtube') {
-      this.youtubeService.getYouTubeVideo(videoId, true, this);
+      this.youtubeService.getYouTubeVideo(videoId)
+      .subscribe(result => {
+        this.showErrorMessage = result;
+        if (!this.showErrorMessage) {
+          this.router.navigate(['/home']);
+        }
+      });
     } else if (platform.value === 'vimeo') {
       this.vimeoService.getVimeoVideo(videoId, true, this);
     }
   }
 
   public onAddDefaultClick(): void {
-    this.vimeoService.getVimeoVideo('172825105', false, this);
-    this.youtubeService.getYouTubeVideo('D2qREDVuGgQ', false, this);
-    this.youtubeService.getYouTubeVideo('5ZwdzeZ-T-s', false, this);
-    this.youtubeService.getYouTubeVideo('ggbtTdcmqtI', false, this);
 
-    setTimeout(() => {
-      this.router.navigate(['/home']);
-    }, 2000);
+    this.youtubeService.getYouTubeVideo('D2qREDVuGgQ')
+      .pipe(
+        switchMap(() => this.youtubeService.getYouTubeVideo('5ZwdzeZ-T-s'))
+        , switchMap(() => this.youtubeService.getYouTubeVideo('ggbtTdcmqtI'))
+      )
+      .subscribe(() => {
+        this.vimeoService.getVimeoVideo('172825105', false, this);
+        this.router.navigate(['/home']);
+      });
   }
 }
